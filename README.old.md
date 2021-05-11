c038e082cc5656b7f8aaa5bbd2ce542cce37cfe6
Alex Keating <keating.dev@protonmail.com>
2021-05-10 21:58:06 -0400

    A good looking form

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# gnosis-guild-app
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,63 @@
-import React, { useCallback, useState } from 'react';
-import styled from 'styled-components';
-import { Button, Loader, Title } from '@gnosis.pm/safe-react-components';
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
+import {
+  Button,
+  Loader,
+  Select,
+  Text,
+  TextField,
+} from "@gnosis.pm/safe-react-components";
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk";
 
-const Container = styled.form`
+const Grid = styled.form`
   margin-bottom: 2rem;
   width: 100%;
-  max-width: 480px;
+  height: 100%;
 
   display: grid;
-  grid-template-columns: 1fr;
-  grid-column-gap: 1rem;
-  grid-row-gap: 1rem;
+  grid-template:
+    "form display" 1fr
+    / 1fr 1fr;
+`;
+
+const GridForm = styled.div`
+  grid-area: form;
+  margin: 0.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const GridDisplay = styled.div`
+  grid-area: display;
+`;
+
+const FormItem = styled.div`
+  margin: 0.4rem;
+`;
+
+const ButtonContainer = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 0.4rem;
+  margin-right: 0.4rem;
 `;
 
 const App: React.FC = () => {
   const { sdk, safe } = useSafeAppsSDK();
   const [submitting, setSubmitting] = useState(false);
+
+  // Input values
+  const [guildName, setGuildName] = useState("");
+  const [guildDescription, setGuildDescription] = useState("");
+  const [contentFormat, setContentFormat] = useState("");
+  const [guildExternalLink, setGuildExternalLink] = useState("");
+
+  const [activeCurrency, setActiveCurrency] = useState("1");
+  const selectItems = [
+    { id: "1", label: "ETH", subLabel: "Minimum amount 0.1" },
+    { id: "2", label: "Dai", subLabel: "Minimum amount 20" },
+  ];
 
   const submitTx = useCallback(async () => {
     setSubmitting(true);
@@ -25,8 +66,8 @@ const App: React.FC = () => {
         txs: [
           {
             to: safe.safeAddress,
-            value: '0',
-            data: '0x',
+            value: "0",
+            data: "0x",
           },
         ],
       });
@@ -39,29 +80,102 @@ const App: React.FC = () => {
     setSubmitting(false);
   }, [safe, sdk]);
 
+  const changeCurrency = (id: string) => {
+    setActiveCurrency(id);
+  };
+
   return (
-    <Container>
-      <Title size="md">{safe.safeAddress}</Title>
-      {submitting ? (
-        <>
-          <Loader size="md" />
-          <br />
+    <Grid>
+      <GridForm>
+        <FormItem>
+          <Text size="xl" strong={true}>
+            Guild Name
+          </Text>
+          <TextField
+            label="50 characters"
+            value={guildName}
+            onChange={(e) => setGuildName(e.target.value)}
+          />
+        </FormItem>
+        <FormItem>
+          <Text size="xl" strong={true}>
+            Guild Description
+          </Text>
+          <TextField
+            label="200 characters"
+            value={guildDescription}
+            onChange={(e) => setGuildDescription(e.target.value)}
+          />
+        </FormItem>
+        <FormItem>
+          <Text size="xl" strong={true}>
+            Content Format for Contributors
+          </Text>
+          <TextField
+            label="E.g. Algorithm-free curation, Design Weekly	Newsletter, Discord"
+            value={contentFormat}
+            onChange={(e) => setContentFormat(e.target.value)}
+          />
+        </FormItem>
+        <FormItem>
+          <Text size="xl" strong={true}>
+            Guild External Link
+          </Text>
+          <TextField
+            label="https://guild.is/"
+            value={guildExternalLink}
+            onChange={(e) => setGuildExternalLink(e.target.value)}
+          />
+        </FormItem>
+        <ButtonContainer>
           <Button
-            size="lg"
-            color="secondary"
-            onClick={() => {
-              setSubmitting(false);
-            }}
+            color={"secondary"}
+            fullWidth={false}
+            size="md"
+            variant="contained"
           >
-            Cancel
+            Upload Image
           </Button>
-        </>
-      ) : (
-        <Button size="lg" color="primary" onClick={submitTx}>
-          Submit
-        </Button>
-      )}
-    </Container>
+        </ButtonContainer>
+        <FormItem>
+          <Text size="xl" strong={true}>
+            Monthly Contributors
+          </Text>
+          <Select
+            activeItemId={activeCurrency}
+            items={selectItems}
+            onItemClick={changeCurrency}
+          />
+        </FormItem>
+        {submitting ? (
+          <>
+            <Loader size="md" />
+            <br />
+            <Button
+              size="lg"
+              color="secondary"
+              onClick={() => {
+                setSubmitting(false);
+              }}
+            >
+              Cancel
+            </Button>
+          </>
+        ) : (
+          <ButtonContainer>
+            <Button
+              size="lg"
+              color="primary"
+              variant="contained"
+              onClick={submitTx}
+            >
+              Create Guild
+            </Button>
+          </ButtonContainer>
+        )}
+      </GridForm>
+      <GridDisplay>here</GridDisplay>
+    </Grid>
   );
 };
 
