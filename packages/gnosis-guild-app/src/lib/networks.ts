2e440153949d437f978005625fe59b53e5cd38f1
Santiago Gonzalez <hernangt12re3@gmail.com>
2021-06-08 08:10:36 -0500

    Deploy a new factory contract

--- packages/gnosis-guild-app/src/lib/networks.ts
+++ packages/gnosis-guild-app/src/lib/networks.ts
@@ -1,6 +1,5 @@
 type Network = {
   name: string;
-  guildAppTemplate: string;
   guildFactory: string;
   daiToken: string;
   subgraphUrl: string;
@@ -12,8 +11,7 @@ type Networks = {
 
 const Default = {
   name: "Rinkeby",
-  guildAppTemplate: "0x14f103b4eaD58431f8B148aaAb4A896B3B16E21b",
-  guildFactory: "0x032001772d52fa4a88b43AAC8dc8B73F28aEF6A4",
+  guildFactory: "0xa99B73A665EF495D61F32138A2cfC49526F28425",
   daiToken: "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea",
   subgraphUrl:
     "https://api.thegraph.com/subgraphs/name/santteegt/gnosis-guild-app-rinkeby"
@@ -22,8 +20,7 @@ const Default = {
 const networks: Networks = {
   4: {
     name: "Rinkeby",
-    guildAppTemplate: "0x14f103b4eaD58431f8B148aaAb4A896B3B16E21b",
-    guildFactory: "0x032001772d52fa4a88b43AAC8dc8B73F28aEF6A4",
+    guildFactory: "0xa99B73A665EF495D61F32138A2cfC49526F28425",
     daiToken: "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea",
     subgraphUrl:
       "https://api.thegraph.com/subgraphs/name/santteegt/gnosis-guild-app-rinkeby"
