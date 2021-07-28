c695eba8a2c7e74ca3001a2bc35984c9d0779fcd
Santiago Gonzalez <hernangt12re3@gmail.com>
2021-07-28 01:06:06 -0500

    Add contracts natspec docs

--- packages/contracts/contracts/guild/GuildApp.sol
+++ packages/contracts/contracts/guild/GuildApp.sol
@@ -16,6 +16,10 @@ import "../interfaces/IGnosisSafe.sol";
 import "../interfaces/IGuild.sol";
 import "../utils/SignatureDecoder.sol";
 
+/// @title GuildApp Contract
+/// @author RaidGuild
+/// @notice Guild app allows you to monetize content and receive recurring subscriptions
+/// @dev uses ERC721 standard to tokenize subscriptions
 contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecoder, IGuild {
     using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
     using SafeERC20Upgradeable for IERC20Upgradeable;
@@ -28,19 +32,25 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         uint256 expirationTimestamp;
     }
 
+    /// @dev flag the contract as initialized
     bool public override initialized;
+    /// @dev flag to keep track if the Guild is accepting subscriptions
     bool public isActive;
+    /// @dev CID of Guild metadata stored on i.e. IPFS
     string public metadataCID;
-    address public tokenAddress; // currently active asset for payments
+    /// @dev current active asset accepted for subscriptions
+    address public tokenAddress;
+    /// @dev current guild subcription price
     uint256 public subPrice;
+    /// @dev subscription period in seconds (i.e. monthly)
     uint256 public subscriptionPeriod;
-
+    /// @dev subscriptions list
     mapping(address => Subscription) public subscriptionByOwner;
-
+    /// @dev assets used for subscription payments
     mapping(address => EnumerableSetUpgradeable.AddressSet) private _approvedTokens;
-
+    /// @dev Gnosis Safe AllowanceModule
     address private _allowanceModule;
-
+    /// @dev next subscriptionID
     uint256 private _nextId;
 
     modifier onlyIfActive() {
@@ -54,10 +64,10 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
     }
 
     event InitializedGuild(address _creator,
-                           address _tokenAddress,
-                           uint256 _subPrice,
-                           uint256 _subscriptionPeriod,
-                           GuildMetadata _metadata);
+        address _tokenAddress,
+        uint256 _subPrice,
+        uint256 _subscriptionPeriod,
+        GuildMetadata _metadata);
     event UpdatedMetadata(string _metadataURI);
     event PausedGuild(bool _isPaused);
     event Withdraw(address _tokenAddress, address beneficiary, uint256 _amount);
@@ -92,6 +102,14 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         initialized = true;
     }
 
+    /// @notice Initialize a new GuildApp contract
+    /// @dev Initialize inherited contracts and perform base GuildApp setup
+    /// @param _creator GuildApp owner
+    /// @param _tokenAddress asset to be accepted for payments
+    /// @param _subPrice subscription price in WEI
+    /// @param _subscriptionPeriod subscription period in seconds
+    /// @param _metadata guild metadata CID
+    /// @param allowanceModule safe module address
     function initialize(address _creator,
                         address _tokenAddress,
                         uint256 _subPrice,
@@ -111,13 +129,25 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         emit InitializedGuild(_creator, _tokenAddress, _subPrice, _subscriptionPeriod, _metadata);
     }
 
+    /// @notice Enable/Disable your GuildApp to accept subscription/payments
+    /// @dev Flag contract as active or not. Only the guild owner can execute
+    /// @param pause boolean to flag the Guild as active
     function pauseGuild(bool pause) external override onlyGuildAdmin {
         require(isActive == pause, "GuildApp: Guild already in that state");
         emit PausedGuild(pause);
         isActive = !pause;
     }
 
-    function withdraw(address _tokenAddress, uint256 _amount, address _beneficiary) public override onlyGuildAdmin {
+    /// @notice Withdraw balance from the Guild
+    /// @dev Only the guild owner can execute
+    /// @param _tokenAddress token asset to withdraw some balance
+    /// @param _amount amount to be withdraw in wei
+    /// @param _beneficiary beneficiary to send funds. If 0x is specified, funds will be sent to the guild owner
+    function withdraw(
+        address _tokenAddress,
+        uint256 _amount,
+        address _beneficiary
+    ) public override onlyGuildAdmin {
         require(_approvedTokens[address(this)].contains(_tokenAddress), "GuildApp: Token has not been approved");
         uint256 outstandingBalance = guildBalance(_tokenAddress);
         require(_amount > 0 && outstandingBalance >= _amount, "GuildApp: Not enough balance to withdraw");
@@ -126,15 +156,30 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         IERC20Upgradeable(tokenAddress).safeTransfer(beneficiary, _amount);
     }
 
-    function updateSubscriptionPrice(address _tokenAddress, uint256 _newSubPrice) public override onlyGuildAdmin onlyIfActive {
-        // require(_tokenAddress != address(0), "GuildApp: Invalid token address");
+    /// @notice Update Guild subscription token and price
+    /// @dev can be executed only by guild owner and if guild is active
+    /// @param _tokenAddress token to be accepted for payments
+    /// @param _newSubPrice new subscription price
+    function updateSubscriptionPrice(
+        address _tokenAddress,
+        uint256 _newSubPrice
+    ) public override onlyGuildAdmin onlyIfActive {
         tokenAddress = _tokenAddress;
         _approvedTokens[address(this)].add(_tokenAddress);
         subPrice = _newSubPrice;
         emit SubscriptionPriceChanged(tokenAddress, subPrice);
     }
 
-    function _validateSignature(address _safeAddress, bytes32 _transferhash, bytes memory _data) internal view {
+    /// @notice Validate signature belongs to a Safe owner
+    /// @dev Currently disabled
+    /// @param _safeAddress Gnosis Safe contract
+    /// @param _transferhash Tx hash used for signing
+    /// @param _data owner signature
+    function _validateSignature(
+        address _safeAddress,
+        bytes32 _transferhash,
+        bytes memory _data
+    ) internal view {
         // Validate signature belongs to a Safe owner
         IGnosisSafe safe = IGnosisSafe(_safeAddress);
         (uint8 v, bytes32 r, bytes32 s) = signatureSplit(_data, 0);
@@ -150,9 +195,17 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         require(safe.isOwner(safeOwner), "GuildApp: Signer is not a safe owner");
     }
 
-    function subscribe(string memory _tokenURI, uint256 _value, bytes memory _data) public payable override onlyIfActive {
+    /// @notice New subscription to the Guild
+    /// @dev Accepts contributions from EOA and Safes w/ enabledAllowanceModule.
+    /// @param _tokenURI URI of subsription metadata
+    /// @param _value subsription payment value send by a user
+    /// @param _data allowance Tx signature used by the safe AllowanceModule
+    function subscribe(
+        string memory _tokenURI,
+        uint256 _value,
+        bytes memory _data
+    ) public payable override onlyIfActive {
         address subscriber = _msgSender();
-        // uint256 value = tokenAddress != address(0) ? _value : msg.value;
         uint256 value = _value;
         if (_data.length == 0) {  // condition if not using a safe
             require((tokenAddress != address(0) && msg.value == 0) ||
@@ -167,9 +220,8 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         }
         require(value >= subPrice, "GuildApp: Insufficient value sent");
         Subscription storage subs = subscriptionByOwner[subscriber];
-        // bool newSubscription = false;
+        
         if (subs.tokenId == 0) {
-            // newSubscription = true;
             _nextId = _nextId.add(1);
             subs.tokenId = _nextId;
             _safeMint(subscriber, subs.tokenId);
@@ -182,43 +234,33 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
             emit RenewSubscription(subs.tokenId, value, subs.expirationTimestamp, _data);
         }
         
-        // Handle payment using EOA allowances
         if (tokenAddress != address(0) && _data.length == 0) {
+            // Handle payment using EOA allowances
             IERC20Upgradeable(tokenAddress).safeTransferFrom(subscriber, address(this), value);
-        } else {
-            require(_allowanceModule != address(0), "GuildApp: Guild does not support Safe Allowances");
-            IAllowanceModule safeModule = IAllowanceModule(_allowanceModule);
-            uint256[5] memory allowance = safeModule.getTokenAllowance(subscriber, address(this), tokenAddress);
-            // allowance.amout - allowance.spent
-            require(allowance[0] - allowance[1] >= value, "GuildApp: Not enough allowance");
-
-            // TODO: Pre-validate if sent by a safe owner. Current issue is related to changing nonce on subsequent transactions
-            // Current solution: only validate on new subscription
-            // if (newSubscription) {
-            //     bytes32 transferHash = safeModule.generateTransferHash(subscriber, // MUST be a safe
-            //                                                            tokenAddress,
-            //                                                            address(this), // to
-            //                                                            uint96(value), // value
-            //                                                            address(0), // paymentToken
-            //                                                            0, // payment
-            //                                                            uint16(allowance[4]) // nonce
-            //                                                            );
-            //     _validateSignature(subscriber, transferHash, _data);
-            // }
-
-            safeModule.executeAllowanceTransfer(
-                subscriber, // MUST be a safe
-                tokenAddress,
-                payable(this), // to
-                uint96(value),
-                address(0), // payment token
-                0, // payment
-                address(this), // delegate
-                "" // bypass signature check as contract signatures are not supported by the module
-            );
+            return;
         }
+        // Else Handle payment using Safe Allowance Module
+        require(_allowanceModule != address(0), "GuildApp: Guild does not support Safe Allowances");
+        IAllowanceModule safeModule = IAllowanceModule(_allowanceModule);
+        uint256[5] memory allowance = safeModule.getTokenAllowance(subscriber, address(this), tokenAddress);
+        // allowance.amout - allowance.spent
+        require(allowance[0] - allowance[1] >= value, "GuildApp: Not enough allowance");
+
+        safeModule.executeAllowanceTransfer(
+            subscriber, // MUST be a safe
+            tokenAddress,
+            payable(this), // to
+            uint96(value),
+            address(0), // payment token
+            0, // payment
+            address(this), // delegate
+            "" // bypass signature check as contract signatures are not supported by the module
+        );
     }
 
+    /// @notice Unsubscribe to the Guild
+    /// @dev NFT token is burned
+    /// @param _tokenId Subscription ID
     function unsubscribe(uint256 _tokenId) public override {
         require(_exists(_tokenId), "GuildApp: Subscription does not exist");
         address subscriber = _msgSender();
@@ -230,6 +272,9 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         emit Unsubscribed(_tokenId);
     }
 
+    /// @notice Get the Guild balance of a specified token
+    /// @param _tokenAddress asset address
+    /// @return current guild balanceOf `_tokenAddres`
     function guildBalance(address _tokenAddress) public view override returns (uint256) {
         if (_approvedTokens[address(this)].contains(_tokenAddress)) {
             if (tokenAddress != address(0)) {
@@ -240,22 +285,40 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         return 0;
     }
 
-    function isSubscriptionOwner(uint256 _tokenId, address _holder) public view override returns (bool) {
+    /// @notice Return if `_holder` owns a subscription with ID `_tokenId`
+    /// @param _tokenId subsription ID
+    /// @param _holder user address
+    /// @return true if `_holder` owns the subscription
+    function isSubscriptionOwner(
+        uint256 _tokenId,
+        address _holder
+    ) public view override returns (bool) {
         return ownerOf(_tokenId) == _holder;
     }
 
+    /// @notice Return true if `_account` has an active subscription
+    /// @param _account subscriber address
+    /// @return true if `_account` has an active subscription
     function hasActiveSubscription(address _account) public view override returns (bool) {
         return subscriptionByOwner[_account].expirationTimestamp > block.timestamp;
     }
 
+    /// @notice Get Subscription ID from `_account`
+    /// @param _account subscriber address
+    /// @return subscription ID that belong to `_account`
     function getSubscriptionIdFor(address _account) public view override returns (uint256) {
         return subscriptionByOwner[_account].tokenId;
     }
 
+    /// @notice Get the subscription expiration timestamp
+    /// @param _account subscriber address
+    /// @return expiration timestamp in seconds
     function getSubscriptionExpiryFor(address _account) external view override returns (uint256) {
         return subscriptionByOwner[_account].expirationTimestamp;
     }
 
+    /// @notice Return list of approved tokens in the guild
+    /// @return array of assets aproved to the Guild
     function approvedTokens() public view override returns (address[] memory) {
         address[] memory tokens = new address[](_approvedTokens[address(this)].length());
         for (uint256 i = 0; i < _approvedTokens[address(this)].length(); i++) {
@@ -264,6 +327,8 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         return tokens;
     }
 
+    /// @notice Return Guild Metadata CID
+    /// @return metadataCID (i.e. IPFS hash)
     function getMetadata() public view override returns (string memory) {
         string memory base = baseURI();
 
@@ -273,6 +338,9 @@ contract GuildApp is ERC721Upgradeable, AccessControlUpgradeable, SignatureDecod
         return string(abi.encodePacked(base, metadataCID));
     }
 
+    /// @notice Set Guild Metadata ID
+    /// @dev Only the guild owner can execute
+    /// @param _metadataCID new metadata CID
     function setMetadata(string memory _metadataCID) external override onlyGuildAdmin onlyIfActive {
         metadataCID = _metadataCID;
         emit UpdatedMetadata(getMetadata());
